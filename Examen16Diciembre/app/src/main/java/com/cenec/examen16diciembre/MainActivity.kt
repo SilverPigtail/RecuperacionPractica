f142eacd3d719c55144a56eb5957edbd3ee70317
SilverPigtail <3512126@gmail.com>
2019-12-16 13:26:50 +0100

    Implementation of Preference class -> Finished Preference class and its implementation to the mainActivity.

--- Examen16Diciembre/app/src/main/java/com/cenec/examen16diciembre/MainActivity.kt
+++ Examen16Diciembre/app/src/main/java/com/cenec/examen16diciembre/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.cenec.examen16diciembre
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ListView
 import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val preferenciasFileName = "Preferencias"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +19,22 @@ class MainActivity : AppCompatActivity() {
 
         //Elimino la sombra de la action bar
         this.supportActionBar?.elevation=0f;
+
+        var valorTipoLista: TextView = findViewById<TextView>(R.id.valorTipoLista)
+
+        val settingsFile = getSharedPreferences(preferenciasFileName, Context.MODE_PRIVATE)
+
+        if(settingsFile.getBoolean("tipolista", false)){
+
+            valorTipoLista.setText("ArrayAdapter").toString()
+
+        }else{
+
+            valorTipoLista.setText("Adapter").toString()
+        }
+
+
+
     }
 
     fun irALista(view: View) {
--- Examen16Diciembre/app/src/main/java/com/cenec/examen16diciembre/PreferenciasActivity.kt
+++ Examen16Diciembre/app/src/main/java/com/cenec/examen16diciembre/PreferenciasActivity.kt
@@ -1,15 +1,21 @@
 package com.cenec.examen16diciembre
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Switch
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_preferencias.*
+import org.w3c.dom.Text
 
 class PreferenciasActivity : AppCompatActivity() {
 
     val preferenciasFileName = "Preferencias"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +23,38 @@ class PreferenciasActivity : AppCompatActivity() {
 
         val settingsFile = getSharedPreferences(preferenciasFileName, Context.MODE_PRIVATE)
 
+
+
+
         var switchLista: Switch = findViewById<Switch>(R.id.switchLista)
+        switchLista.isChecked = settingsFile.getBoolean("tipolista", true)
 
-        val cambioLista: TextView = findViewById<TextView>(R.id.valorTipoLista)
+       /* if(switchLista.isChecked){
 
-        switchLista.isChecked = settingsFile.getBoolean("tipolista", false)
+            cambioLista.setText("Mi adapter")
+        }else{
 
+            cambioLista.setText("ArrayAdapter")
+        }*/
 
+    }
 
+    fun switchActivated(view: View){
+
+        val settingsFile = getSharedPreferences(preferenciasFileName, Context.MODE_PRIVATE)
+
+        var myEditor: SharedPreferences.Editor = settingsFile.edit()
+
+        if(switchLista.isChecked){
+
+            myEditor.putBoolean("tipolista", true)
+
+        } else {
+
+            myEditor.putBoolean("tipolista", false)
+
+        }
+
+        myEditor.apply()
     }
 }
